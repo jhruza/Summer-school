2977a109a04ce1add67cb9217fccdfb6230db62a
jhruza <johannes.hruza@sund.ku.dk>
2024-07-17 19:15:58 +0200

    added PPS

--- R/lasso_final.R
+++ R/lasso_final.R
@@ -2,6 +2,7 @@ library(glmnet)
 library(dplyr)
 library(ISLR)
 library(ggfortify)
+set.seed(4)
 
 
 load('Data/df_final.Rda')
@@ -18,7 +19,7 @@ glmmod = glmnet(x, df$outcome, alpha=1, family="binomial")
 plot(glmmod, xvar="lambda")
 
 # Cross validazione!
-set.seed(4)
+
 lambdas = c(10^seq(10, -2, length.out = 100), 0)
 cv.model_lasso = cv.glmnet(x, df$outcome, lambda = lambdas, alpha = 1, nfolds = 10)
 autoplot(cv.model_lasso)
@@ -39,6 +40,7 @@ set.seed(1)
 modelr <- cv.glmnet(x, df$outcome, alpha = 0)
 coefr <- as.matrix(coef(modelr, s = modelr$lambda.min))
 w.r <- 1/(abs(coefr[-1,]))^g
+
 #adaptive elastic net
 cv.alasso <- cv.glmnet(x, df$outcome, alpha=0.5, family="binomial", penalty.factor = w.r)
 cbind(coef(mod, s="lambda.min"),
@@ -48,3 +50,11 @@ alasso <- glmnet(x, df$outcome, alpha=0.5, family="binomial", lambda = cv.alasso
 alasso$beta@i # variables indexes
 alasso$beta@x # coefficients values
 alasso$beta@Dimnames[[1]][alasso$beta@i] #names
+
+
+###protein prognostic score PPS
+formula_string <- paste("outcome ~", paste(alasso$beta@Dimnames[[1]][alasso$beta@i], collapse = " + "))
+formula <- as.formula(formula_string)
+PSS.model  <- glm(formula_string, data = df, family = "binomial")
+PPS <- predict(PSS.model, df[ ,alasso$beta@Dimnames[[1]][alasso$beta@i]], type= "response" )
+hist(PPS, breaks=30)
